1e31e3969ec92ae8d5aa5265a0ca01f16c901d23
mirfanm2019 <224185431+mirfanm2019@users.noreply.github.com>
2025-12-25 09:46:56 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,12 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+
 #judul halaman
 st.title("Dashboard Data Sepeda-Tugas Machine Learning GDG")
-st.sidebar("M. Irfan Maulana-Teknik Informatika 2025")
+st.sidebar.write("M. Irfan Maulana-Teknik Informatika 2025")
+
 #load data
 df= pd.read_csv("day.csv")
 df['dteday']=pd.to_datetime(df['dteday'])
+
 #Tabel data
 if st.checkbox("Tampilkan Data Mentah"):
     st.write(df)
@@ -16,12 +19,12 @@ if st.checkbox("Tampilkan Data Mentah"):
 st.subheader(" Tren Penyewaan Sepeda (2011-2012)")
 fig1, ax1=plt.subplots(figsize=(16,8))
 #Gambar garis
-ax1.plot(df["dtedat"], df["cnt"], color="#0F4A91")
+ax1.plot(df["dteday"], df["cnt"], color="#0F4A91")
 #Memberi label
-ax1.setxLabel("Tanggal")
-ax1.setyLabel("Jumlah Sewa")
+ax1.set_xlabel("Tanggal")
+ax1.set_ylabel("Jumlah Sewa")
 st.pyplot(fig1)
-t.write("**Insight:** Terjadi peningkatan signifikan di tahun 2012 dibandingkan 2011, dengan puncak di pertengahan tahun.")
+st.write("**Insight:** Terjadi peningkatan signifikan di tahun 2012 dibandingkan 2011, dengan puncak di pertengahan tahun.")
 
 #Grafik 2
 avg_sewa=df.groupby('workingday')['cnt'].mean()
@@ -33,4 +36,3 @@ ax2.set_ylabel("Rata-rata penyewa")
 
 st.pyplot(fig2)
 st.write("**Insight:** Rata-rata penyewaan lebih tinggi pada hari kerja, menunjukkan penggunaan utama untuk transportasi rutin.")
-
